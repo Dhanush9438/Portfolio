8245a7641633395c7347c0695dd8d308da79298b
dhanush <dhanushcse12@gmail.com>
2023-02-06 15:52:14 +0530

    init

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -27,9 +27,9 @@ export const Certifications = () => {
       name="Certifications"
       className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">
           Certifications
           </p>
           <p className="py-6">Check out some of my work right here</p>
